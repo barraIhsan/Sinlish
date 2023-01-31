5d93cf5a3eb2d5eca35dabb94aa032d56ae107eb
barraIhsan <57800056+barraIhsan@users.noreply.github.com>
2023-01-31 08:11:42 +0700

    feat: add splash screen

---
+++ app/src/main/java/com/sinlish/SplashScreen.kt
@@ -0,0 +1,24 @@
+package com.sinlish
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashScreen : AppCompatActivity() {
+
+    // Membuat variable untuk berapa lama splash screennya muncul (milisecond)
+    private val splashTime = 3000L
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        // Setelah delay selama slashTime variable
+        Handler().postDelayed({
+            // Maka pindah ke activity Login
+            startActivity(Intent(this, HomeScreen::class.java))
+            finish()
+        }, splashTime)
+    }
+}
