e09923950795a188abd034d071349a710ef87714
barraIhsan <57800056+barraIhsan@users.noreply.github.com>
2023-02-02 08:36:28 +0700

    feat: switch screen when cardview clicked

--- app/src/main/java/com/sinlish/HomeScreen.kt
+++ app/src/main/java/com/sinlish/HomeScreen.kt
@@ -1,11 +1,30 @@
 package com.sinlish
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.cardview.widget.CardView
+import com.sinlish.menus.QuizScreen
+import com.sinlish.menus.StoryScreen
+import com.sinlish.menus.VocabScreen
 
 class HomeScreen : AppCompatActivity() {
+
+    private lateinit var quiz: CardView
+    private lateinit var story: CardView
+    private lateinit var vocab: CardView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        quiz = findViewById(R.id.quiz)
+        story = findViewById(R.id.story)
+        vocab = findViewById(R.id.vocab)
+
+        quiz.setOnClickListener { startActivity(Intent(this, QuizScreen::class.java)) }
+        story.setOnClickListener { startActivity(Intent(this, StoryScreen::class.java)) }
+        vocab.setOnClickListener { startActivity(Intent(this, VocabScreen::class.java)) }
+
     }
 }
