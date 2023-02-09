855cc21895737f333a29b73e7f9b524becbec47f
haritsalahza <123914725+haritsalahza@users.noreply.github.com>
2023-02-09 09:12:08 +0700

    fix: story string

---
+++ app/src/main/java/com/sinlish/menus/story/StoryData.kt
@@ -0,0 +1,14 @@
+package com.sinlish.menus.story
+
+object StoryData {
+    private val storyTitle = arrayOf(
+        "A Glass Envelope",
+    )
+
+    private val storyContent = arrayOf(
+        "My daughter, Jane, never\ndreamed of receiving a letter\nfrom a girl of her own age in\nHolland. Last year, we were\ntraveling across the Channel\nand Jane put a piece of paper\nwith her name and address on\nit into a bottle. She threw the\nbottle into the sea. She never\nthought of it again, but ten\nmonth later, she received a\nletter from a girl in Holland.\nBoth girls write to each other\nregularly now. However, they\nhave decided to use the post\noffice. Letters will cost a little\nmore, but they will certainly\ntravel faster."
+
+    )
+
+
+}
