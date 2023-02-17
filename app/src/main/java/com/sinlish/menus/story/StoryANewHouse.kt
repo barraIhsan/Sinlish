0163e012fbd45ca1b14122f57a2bde59e9d5cffe
haritsalahza <123914725+haritsalahza@users.noreply.github.com>
2023-02-17 09:34:26 +0700

    add : screen

---
+++ app/src/main/java/com/sinlish/menus/story/StoryANewHouse.kt
@@ -0,0 +1,12 @@
+package com.sinlish.menus.story
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sinlish.R
+
+class StoryANewHouse : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_story_anew_house)
+    }
+}
